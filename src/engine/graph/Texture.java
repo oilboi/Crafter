fdba5dec68ecced48a079596eb300309995b53b2
oilboi <47129783+oilboi@users.noreply.github.com>
2020-08-19 19:07:08 -0400

    Fix texture filtering

--- src/engine/graph/Texture.java
+++ src/engine/graph/Texture.java
@@ -59,8 +59,7 @@ public class Texture {
         //tell openGL how to unpack the RGBA bytes. Each component is 1 byte in size
         glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
 
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-        //glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
 
         //upload the texture data
         glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, buf);

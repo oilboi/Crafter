7ade5a48acd17ea7f1f52630aa6c14cb3a1de25b
oilboi <47129783+oilboi@users.noreply.github.com>
2020-09-01 08:41:40 -0400

    Fix collision detection

--- src/game/collision/Collision.java
+++ src/game/collision/Collision.java
@@ -24,7 +24,7 @@ public class Collision {
         pos.y += inertia.y * gameSpeed;
         pos.z += inertia.z * gameSpeed;
 
-        onGround = collisionDetect(pos, inertia, onGround, width, height);
+        onGround = collisionDetect(pos, inertia, width, height);
 
         //apply friction
         inertia.x += -inertia.x * gameSpeed * 10; // do (10 - 9.5f) for slippery!
@@ -37,26 +37,23 @@ public class Collision {
         return onGround;
     }
 
-    private static boolean collisionDetect(Vector3f pos, Vector3f inertia, boolean onGround, float width, float height){
-        onGround = false;
+    private static boolean collisionDetect(Vector3f pos, Vector3f inertia, float width, float height){
+        boolean onGround = false;
 
         //get the real positions of the blocks
         Vector3f fPos = floorPos(pos);
 
-
-//        if (detectBlock(new Vector3f(fPos.x, fPos.y+1f, fPos.z))){
-//            pos.y += 1f;
-//        }
-
-
         int y = 2;
         int x = -1;
         int z = -1;
+
+        Vector3f oldPos = new Vector3f(pos);
+        Vector3f oldInertia = new Vector3f(inertia);
+
         //collide with all blocks in local area
         for (int i = 0; i < 36; i++){
             if (detectBlock(new Vector3f(fPos.x + x, fPos.y + y, fPos.z + z))) {
-                CustomAABB us = new CustomAABB(pos.x, pos.y, pos.z, width, height);
-                onGround = collide(us, new CustomBlockBox((int) fPos.x + x, (int) fPos.y + y, (int) fPos.z + z), pos, inertia, width, height, onGround);
+                onGround = collide(new CustomBlockBox((int) fPos.x + x, (int) fPos.y + y, (int) fPos.z + z), pos, inertia, width, height, onGround);
             }
             y--;
             if (y < -1){
@@ -69,6 +66,14 @@ public class Collision {
             }
         }
 
+        //final touches to make sure player is not clipping
+        if(pos.y < oldPos.y){
+            if(pos.x != oldPos.x || pos.z != oldPos.z){
+                pos.y = oldPos.y;
+                inertia.y = oldInertia.y;
+            }
+        }
+
         return onGround;
     }
 
@@ -76,11 +81,13 @@ public class Collision {
     private final static float FOOT_ADJUSTMENT_HEIGHT = 0.3f;
     private final static float HEAD_ADJUSTMENT_HEIGHT = 0.6f;
     //this is where actual collision events occur!
-    public static boolean collide(CustomAABB us, CustomBlockBox block, Vector3f pos, Vector3f inertia, float width, float height, boolean onGround){
+    public static boolean collide(CustomBlockBox block, Vector3f pos, Vector3f inertia, float width, float height, boolean onGround){
+
+        CustomAABB us = new CustomAABB(pos.x, pos.y, pos.z, width, height);
+
         boolean within;
 
-        within = !(us.getLeft()   > block.getRight() || us.getRight() < block.getLeft() || us.getBottom() > block.getTop()   || us.getTop()   < block.getBottom() || us.getFront()  > block.getBack()  || us.getBack()  < block.getFront());
-
+        within = !(us.getLeft() > block.getRight() || us.getRight() < block.getLeft() || us.getBottom() > block.getTop() || us.getTop() < block.getBottom() || us.getFront() > block.getBack() || us.getBack() < block.getFront());
         //floor detection
         if (within) {
             if (block.getTop() > us.getBottom() && inertia.y < 0 && us.getBottom() - block.getTop() > -0.15f) {
@@ -91,68 +98,45 @@ public class Collision {
             }
         }
 
-        within = !(us.getLeft()+0.1f > block.getRight() || us.getRight()-0.1f < block.getLeft() || us.getBottom() > block.getTop()   || us.getTop()   < block.getBottom() || us.getFront()+0.1f  > block.getBack()  || us.getBack()-0.1f  < block.getFront());
+        us.updatePos(pos);
 
+        within = !(us.getLeft() > block.getRight() || us.getRight() < block.getLeft() || us.getBottom() > block.getTop() || us.getTop() < block.getBottom() || us.getFront() > block.getBack() || us.getBack() < block.getFront());
         //head detection
         if (within) {
             if (block.getBottom() < us.getTop() && inertia.y >= 0 && us.getTop() - block.getBottom() < 0.15f) {
-                pos.y = block.getBottom() - height - 0.0001f;
+                pos.y = block.getBottom() - height - 0.001f;
                 inertia.y = 0;
             }
         }
+
+        us.updatePos(pos);
+
         float averageX = Math.abs(((block.getLeft() + block.getRight())/2f) - pos.x);
         float averageZ = Math.abs(((block.getFront() + block.getBack())/2f) - pos.z);
 
-        if (averageX > averageZ) {
-            if (!detectBlock(new Vector3f(block.getLeft()+1, block.getBottom(),block.getFront()))) {
-
-                us = new CustomAABB(pos.x, pos.y + FOOT_ADJUSTMENT_HEIGHT, pos.z, width, height - HEAD_ADJUSTMENT_HEIGHT);
-                within = !(us.getLeft() > block.getRight() || us.getRight() < block.getLeft() || us.getBottom() > block.getTop() || us.getTop() < block.getBottom() || us.getFront() > block.getBack() || us.getBack() < block.getFront());
-
+        within = !(us.getLeft() > block.getRight() || us.getRight() < block.getLeft() || us.getBottom() > block.getTop() || us.getTop() < block.getBottom() || us.getFront() > block.getBack() || us.getBack() < block.getFront());
+        if (within) {
+            if (averageX > averageZ) {
                 //x- detection
-                if (within) {
-                    if (block.getRight() > us.getLeft() && inertia.x < 0) {
-                        pos.x = block.getRight() + width + 0.00001f;
-                        inertia.x = 0;
-                    }
+                if (block.getRight() > us.getLeft() && inertia.x < 0) {
+                    pos.x = block.getRight() + width + 0.001f;
+                    inertia.x = 0;
                 }
-            }
-            if (!detectBlock(new Vector3f(block.getLeft()-1, block.getBottom(),block.getFront()))) {
-                us = new CustomAABB(pos.x, pos.y + FOOT_ADJUSTMENT_HEIGHT, pos.z, width, height - HEAD_ADJUSTMENT_HEIGHT);
-                within = !(us.getLeft() > block.getRight() || us.getRight() < block.getLeft() || us.getBottom() > block.getTop() || us.getTop() < block.getBottom() || us.getFront() > block.getBack() || us.getBack() < block.getFront());
-
                 //x+ detection
-                if (within) {
-                    if (block.getLeft() < us.getRight() && inertia.x > 0) {
-                        pos.x = block.getLeft() - width - 0.00001f;
-                        inertia.x = 0;
-                    }
+                if (block.getLeft() < us.getRight() && inertia.x > 0) {
+                    pos.x = block.getLeft() - width - 0.001f;
+                    inertia.x = 0;
                 }
-            }
-        } else {
-            if (!detectBlock(new Vector3f(block.getLeft(), block.getBottom(),block.getFront()+1))) {
-                us = new CustomAABB(pos.x, pos.y + FOOT_ADJUSTMENT_HEIGHT, pos.z, width, height - HEAD_ADJUSTMENT_HEIGHT);
-                within = !(us.getLeft() > block.getRight() || us.getRight() < block.getLeft() || us.getBottom() > block.getTop() || us.getTop() < block.getBottom() || us.getFront() > block.getBack() || us.getBack() < block.getFront());
-
+            } else {
                 //z- detection
-                if (within) {
-                    if (block.getBack() > us.getFront() && inertia.z < 0) {
-                        pos.z = block.getBack() + width + 0.00001f;
-                        inertia.z = 0;
-                    }
+                if (block.getBack() > us.getFront() && inertia.z < 0) {
+                    pos.z = block.getBack() + width + 0.001f;
+                    inertia.z = 0;
                 }
-            }
-
-            if (!detectBlock(new Vector3f(block.getLeft(), block.getBottom(),block.getFront()-1))) {
-                us = new CustomAABB(pos.x, pos.y + FOOT_ADJUSTMENT_HEIGHT, pos.z, width, height - HEAD_ADJUSTMENT_HEIGHT);
-                within = !(us.getLeft() > block.getRight() || us.getRight() < block.getLeft() || us.getBottom() > block.getTop() || us.getTop() < block.getBottom() || us.getFront() > block.getBack() || us.getBack() < block.getFront());
-
-                //z+ detection
-                if (within) {
-                    if (block.getFront() < us.getBack() && inertia.z > 0) {
-                        pos.z = block.getFront() - width - 0.00001f;
-                        inertia.z = 0;
-                    }
+                 //z+ detection
+                if (block.getFront() < us.getBack() && inertia.z > 0) {
+                    pos.z = block.getFront() - width - 0.001f;
+                    inertia.z = 0;
                 }
             }
         }
@@ -168,9 +152,6 @@ public class Collision {
 
     //this is used for block placing
     public static boolean wouldCollide(CustomAABB us, CustomBlockBox block){
-        boolean xWithin = !(us.getLeft()   > block.getRight() || us.getRight() < block.getLeft());
-        boolean yWithin = !(us.getBottom() > block.getTop()   || us.getTop()   < block.getBottom());
-        boolean zWithin = !(us.getFront()  > block.getBack()  || us.getBack()  < block.getFront());
-        return (xWithin && yWithin && zWithin);
+        return !(us.getLeft() > block.getRight() || us.getRight() < block.getLeft() || us.getBottom() > block.getTop() || us.getTop() < block.getBottom() || us.getFront() > block.getBack() || us.getBack() < block.getFront());
     }
 }
--- src/game/collision/CustomAABB.java
+++ src/game/collision/CustomAABB.java
@@ -1,14 +1,32 @@
 package game.collision;
 
+import org.joml.Vector3f;
+
 public class CustomAABB {
 
     private float[] aabb;
-
+    private float width;
+    private float height;
     public CustomAABB(float x, float y, float z, float width, float height){
 
         aabb = new float[]{x-width, y, z-width, x+width, y+height, z+width};
 
+        this.width = width;
+        this.height = height;
         //                    0     1     2        3        4         5
+    }
+
+    public void updatePos(Vector3f pos){
+        aabb[0] = pos.x-width;
+        aabb[1] = pos.y;
+        aabb[2] = pos.z-width;
+        aabb[3] = pos.x+width;
+        aabb[4] = pos.y+height;
+        aabb[5] = pos.z+width;
+    }
+
+    public void updateWidth(float width){
+        this.width = width;
     }
 
     public float getLeft(){

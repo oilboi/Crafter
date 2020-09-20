c8b7389681db4ed0ebc95a499f927507fbe53765
oilboi <47129783+oilboi@users.noreply.github.com>
2020-09-20 04:26:58 -0400

    Invert inventory hotbar location

---
+++ src/game/player/Inventory.java
@@ -0,0 +1,8 @@
+package game.player;
+
+public class Inventory {
+    private static String[][] inventoryNames = new String[4][9];
+    private static int[][] inventoryNumbers = new int[4][9];
+
+    
+}

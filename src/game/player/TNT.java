4a79bdd0397574ed3bef978dec442674b5320789
oilboi <47129783+oilboi@users.noreply.github.com>
2020-09-30 20:58:22 -0400

    Clean up TNT Class

--- src/game/player/TNT.java
+++ src/game/player/TNT.java
@@ -1,7 +1,5 @@
 package game.player;
 
-import game.blocks.BlockDefinition;
-import jdk.nashorn.internal.ir.Block;
 import org.joml.Vector3f;
 
 import static game.blocks.BlockDefinition.getBlockDefinition;
@@ -10,69 +8,27 @@ import static game.chunk.Chunk.setBlock;
 import static engine.FancyMath.getDistance;
 import static game.item.ItemEntity.createItem;
 import static engine.TNTEntity.createTNT;
-//import static game.light.Light.floodFill;
 
 public class TNT {
-    private static int[][] chunkBuffer;
-    private static int chunkBufferIndex;
-    private static int currentChunkX;
-    private static int currentChunkZ;
-    private static int currentPosX;
-    private static int currentPosZ;
-    private static int currentBlock;
-    private static boolean found = false;
-    private static int x,y,z;
 
-    public static void boom(int posX, int posY, int posZ, int boomDistance) throws Exception {
-        chunkBufferIndex = 0;
-        chunkBuffer = new int[boomDistance][2];
-
-        for (x = posX - boomDistance; x < posX + boomDistance; x++) {
-            for (y = posY - boomDistance; y < posY + boomDistance; y++) {
-                for (z = posZ - boomDistance; z < posZ + boomDistance; z++) {
-
-                    currentChunkX = (int)(Math.floor((float) x / 16f));
-                    currentChunkZ = (int)(Math.floor((float) z / 16f));
-
-                    currentPosX = x - (16 * currentChunkX);
-                    currentPosZ = z - (16 * currentChunkZ);
-
-                    if (getDistance(posX, posY, posZ, x, y, z) <= boomDistance) {
-
-                        currentBlock = getBlock(currentPosX, y, currentPosZ, currentChunkX, currentChunkZ);
-
+    public static void boom(Vector3f pos, int boomDistance) throws Exception {
+        for (int x = (int)Math.floor(pos.x) - boomDistance; x < (int)Math.floor(pos.x) + boomDistance; x++) {
+            for (int y = (int)Math.floor(pos.y) - boomDistance; y < (int)Math.floor(pos.y) + boomDistance; y++) {
+                for (int z = (int)Math.floor(pos.z) - boomDistance; z < (int)Math.floor(pos.z) + boomDistance; z++) {
+                    if (getDistance(pos.x, pos.y, pos.z, x, y, z) <= boomDistance) {
+                        int currentBlock = getBlock(x, y, z);
                         //don't destroy bedrock
                         if(currentBlock != 5) {
-                            setBlock(currentPosX, y, currentPosZ, currentChunkX, currentChunkZ, (short) 0);
-                            //todo: make this an API callback!!
+                            setBlock(x, y, z, 0);
                             if (currentBlock != 0 && currentBlock != 6 && Math.random() > 0.98) {
-                                createItem(getBlockDefinition(currentBlock).name, new Vector3f(currentPosX+(currentChunkX*16), y, currentPosZ+(currentChunkZ*16)), 1);
+                                createItem(getBlockDefinition(currentBlock).name, new Vector3f(x,y,z), 1);
                             } else if (currentBlock == 6){
                                 createTNT(new Vector3f(x, y, z), (float)(1f+Math.random()), false);
                             }
-
                         }
-
-                        //add chunks to chunk generation buffer ID: 555
-                        found = false;
-                        for (int[] index : chunkBuffer) {
-                            if (index[0] == currentChunkX && index[1] == currentChunkZ) {
-                                found = true;
-                            }
-                        }
-                        if (!found) {
-                            chunkBuffer[chunkBufferIndex] = new int[]{currentChunkX, currentChunkZ};
-                            chunkBufferIndex++;
-                        }
-                        //end: ID: 555
                     }
                 }
             }
-        }
-
-        for (int[] index : chunkBuffer) {
-//            floodFill(index[0], index[1]);
-//            generateChunkMesh(index[0], index[1], false);
         }
     }
 }
